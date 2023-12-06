96fb908d74c5119971072e85b9455d21e173576c
Hannes <hannes-bachmann@t-online.de>
2023-12-06 14:17:29 +0100

    solution for december sixth in advent of code, second task

--- december_06.py
+++ december_06.py
@@ -7,7 +7,9 @@ def load_codes() -> list[str]:
     lines = [l.replace('\n', '') for l in lines]
     return lines
 
-
+##################################################
+##################### PART 1 #####################
+##################################################
 def parse_times_and_distance(input_lines: list[str]) -> tuple[list[int], list[int]]:
     times = [int(i) for i in re.findall(r'\d+', re.sub(r'Time: +', '', input_lines[0]))]
     distances = [int(i) for i in re.findall(r'\d+', re.sub(r'Distance: +', '', input_lines[1]))]
@@ -33,7 +35,39 @@ def calc_check_value(input_lines: list[str]) -> int:
     return check_value
 
 
+##################################################
+##################### PART 2 #####################
+##################################################
+def parse_time_distance_p2(input_lines: list[str]) -> tuple[int, int]:
+    time = int(re.sub(r'Time: +', '', input_lines[0]).replace(' ', ''))
+    distance = int(re.sub(r'Distance: +', '', input_lines[1]).replace(' ', ''))
+    return time, distance
+
+
+def number_of_options(input_lines: list[str]) -> int:
+    time, distance = parse_time_distance_p2(input_lines=input_lines)
+    # time_hold * (time - time_hold) must be higher than distance
+    # strategy: calculate until the first value is higher.
+    #           then search from the last time value to get upper limit
+    first_success = 0
+    for time_hold in range(time + 1):
+        if time_hold * (time - time_hold) > distance:
+            first_success = time_hold
+            break
+    t_tmp = time
+    last_success = time
+    while t_tmp >= 0:
+        if t_tmp * (time - t_tmp) > distance:
+            last_success = t_tmp
+            break
+        t_tmp -= 1
+    return last_success - first_success + 1
+
+
 if __name__ == '__main__':
     input_lines = load_codes()
+    no = number_of_options(input_lines=input_lines)
+    print(no)
+
     print(calc_check_value(input_lines=input_lines))
     pass
